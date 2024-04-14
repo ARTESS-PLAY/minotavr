d9afcdee54299bfbf42bd8e713000e302edb2c3a
pan0mera <artess.ponomarev@mail.ru>
2024-04-15 01:14:45 +0300

    Добавил константы

---
+++ src/utils/constants.ts
@@ -0,0 +1,15 @@
+export const TILES = {
+    LABYRINTH: 'labyrinth',
+};
+
+export const SIZES = {
+    TILE: {
+        WIDTH: 32,
+        HEIGTH: 32,
+    },
+};
+
+export const LAYERS = {
+    GROUNG: 'ground',
+    WALLS: 'walls',
+};
