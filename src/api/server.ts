d0564e12de3d9e065e7be47bfbf8bcd6f107d485
pan0mera <artess.ponomarev@mail.ru>
2024-06-20 01:17:40 +0300

    закоментировал код неиспользуемый

--- src/api/server.ts
+++ src/api/server.ts
@@ -5,20 +5,20 @@ const SERVERURL = 'https://minotavr-game.ru';
 /**
  * Получает json карту с сервера
  */
-export const getMapFromServer = async () => {
-    const res = await axios.get(`${SERVERURL}/map/map.json`);
+// export const getMapFromServer = async () => {
+//     const res = await axios.get(`${SERVERURL}/map/map.json`);
 
-    console.log(res);
-};
+//     console.log(res);
+// };
 
-/**
- * Получает картинки для тайлов для карты с сервера
- */
-export const getMapTiledImageFromServer = async () => {
-    const res = await axios.get(`${SERVERURL}/map/durotar.jpg`);
+// /**
+//  * Получает картинки для тайлов для карты с сервера
+//  */
+// export const getMapTiledImageFromServer = async () => {
+//     const res = await axios.get(`${SERVERURL}/map/durotar.jpg`);
 
-    console.log(res);
-};
+//     console.log(res);
+// };
 
 // Прямая ссылка на карту с сервера
 export const MAP_SERVER_JSON_URL = SERVERURL + '/map/map.json';
